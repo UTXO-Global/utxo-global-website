0fa9ed586e43ca6a97967d60e3a1149dd55f3fbd
yangiv <khoahua.yiv@gmail.com>
2024-09-09 16:34:25 +0700

    chore(bounty-contest): update leaderboard

--- configs/bug-report.ts
+++ configs/bug-report.ts
@@ -1,46 +1,46 @@
 import { BountyContestLeaderboardItemType } from "@/types/common";
 
-export const leaderboardData: BountyContestLeaderboardItemType[] =[
+export const leaderboardData: BountyContestLeaderboardItemType[] = [
   {
-      "rank": 1,
-      "name": "Whitehat Hacker",
-      "email": "rosaxxxa320@gmail.com",
-      "points": 300
+    rank: 1,
+    name: "Whitehat Hacker",
+    email: "rosaxxxa320@gmail.com",
+    points: 300,
   },
   {
-      "rank": 2,
-      "name": "w3bbh",
-      "email": "w3xxxh@outlook.com",
-      "points": 100
+    rank: 2,
+    name: "rutonboy",
+    email: "fredyxxxxyah249@gmail.com",
+    points: 125,
   },
   {
-      "rank": 2,
-      "name": "Tea",
-      "email": "coppexxxt05@gmail.com",
-      "points": 100
+    rank: 3,
+    name: "w3bbh",
+    email: "w3xxxh@outlook.com",
+    points: 100,
   },
   {
-      "rank": 2,
-      "name": "rutonboy",
-      "email": "fredyxxxxyah249@gmail.com",
-      "points": 100
+    rank: 3,
+    name: "Tea",
+    email: "coppexxxt05@gmail.com",
+    points: 100,
   },
   {
-      "rank": 5,
-      "name": "ouhua",
-      "email": "ouxxxa@gmail.com",
-      "points": 75
+    rank: 5,
+    name: "ouhua",
+    email: "ouxxxa@gmail.com",
+    points: 75,
   },
   {
-      "rank": 6,
-      "name": "Sam",
-      "email": "saxxxynh@gmail.com",
-      "points": 50
+    rank: 6,
+    name: "Sam",
+    email: "saxxxynh@gmail.com",
+    points: 50,
   },
   {
-      "rank": 6,
-      "name": "Kaushik",
-      "email": "kowxxxhu@gmail.com",
-      "points": 50
-  }
+    rank: 6,
+    name: "Kaushik",
+    email: "kowxxxhu@gmail.com",
+    points: 50,
+  },
 ];
