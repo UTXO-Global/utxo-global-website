e394e48f3d00b6478a493a6ffaee515f36d93679
KhoiNguyen <nguyen.nguyen@apps-cyclone.com>
2024-10-17 08:37:17 +0700

    feat(indexer): update doc link

--- configs/common.ts
+++ configs/common.ts
@@ -7,17 +7,14 @@ export const EXTENTSION_GITHUB = "https://github.com/UTXO-Global/utxo-wallet-ext
 export const MEDIA_KIT_GITHUB = "https://github.com/UTXO-Global/utxo-global-media-kit";
 export const SUBSTACK_LINK = "https://utxoglobal.substack.com/";
 export const DOC_LINK = "https://utxo-global.gitbook.io/utxo-global";
-export const CHROME_EXTENSION_LINK =
-  "https://chromewebstore.google.com/detail/utxo-global-wallet/lnamkkidoonpeknminiadpgjiofpdmle";
+export const CHROME_EXTENSION_LINK = "https://chromewebstore.google.com/detail/utxo-global-wallet/lnamkkidoonpeknminiadpgjiofpdmle";
 export const NERVOS_LINK = "https://www.nervos.org/wallets#utxo_global_wallet";
 export const CKBCCC_DEMO_LINK = "https://ckbccc-demo.vercel.app/";
 export const BUG_REPORT_LINK = "https://forms.gle/3Y4HSpLoY7nAVExz7";
 export const REQUEST_API_ACCESS_LINK =
   "https://docs.google.com/forms/d/e/1FAIpQLSf4h0LPs3_ZEZ4YZOO4-5B45br0dfLt-HSMOeBOdaM-CcKuCA/viewform";
-export const INDEXER_COIN_API_LINK =
-  "https://utxo-global.gitbook.io/utxo-global/ckb-advanced-indexer-api/api-reference/coins";
-export const INDEXER_NFT_API_LINK =
-  "https://utxo-global.gitbook.io/utxo-global/ckb-advanced-indexer-api/api-reference/nfts";
+export const INDEXER_COIN_API_LINK = `${DOC_LINK}/ckb-advanced-indexer-api/api-reference/coins`;
+export const INDEXER_NFT_API_LINK = `${DOC_LINK}/ckb-advanced-indexer-api/api-reference/nfts`;
 export const BUG_REPORT_RESULT_LINK = "";
 export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACIKING_ID;
 export const MULTI_SIG_LINK = process.env.NEXT_PUBLIC_MULTI_SIG_LINK || "";
