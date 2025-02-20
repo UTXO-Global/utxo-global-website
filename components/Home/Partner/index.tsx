4545110000708760826acba5197239309216e138
KhoiNguyen <nguyen.nguyen@apps-cyclone.com>
2025-02-20 16:51:40 +0700

    feat(home): add nervape partner

--- components/Home/Partner/index.tsx
+++ components/Home/Partner/index.tsx
@@ -1,7 +1,12 @@
 /* eslint-disable @next/next/no-img-element */
 import { useTranslation } from "next-export-i18n";
+import cn from "@/utils/cn";
 
 const PARTNERS = [
+  {
+    name: "Nervape",
+    image: "/images/nervape.svg",
+  },
   {
     name: "d.id",
     image: "/images/d.id.png",
@@ -34,17 +39,25 @@ const Partners = () => {
           {t("partners")}
         </h3>
         <div className="flex flex-wrap justify-center gap-4 md:gap-8 mt-10 md:mt-20">
-          {PARTNERS.map((z, i) => (
-            <div
-              key={z.name}
-              className="flex cursor-pointer border-[#F5F5F5]  border rounded-2xl gap-2 md:gap-4 sm:gap-3 p-2 sm:px-4 sm:py-2 lg:py-3 lg:px-5 items-center group"
-            >
-              <img src={z.image} alt={z.name} className="w-8 md:w-10 lg:w-[52px] group-hover:grayscale-0 grayscale transition-all" />
-              <div className="text-base sm:text-2xl font-medium lg:text-4xl text-grey-200 group-hover:text-dark-100 transition-all">
-                {z.name}
+          {PARTNERS.map((z, i) => {
+            return (
+              <div
+                key={z.name}
+                className="flex cursor-pointer border-[#F5F5F5]  border rounded-2xl gap-2 md:gap-4 sm:gap-3 p-2 sm:px-4 sm:py-2 lg:py-3 lg:px-5 items-center group"
+              >
+                <img
+                  src={z.image}
+                  alt={z.name}
+                  className={cn("w-8 md:w-10 lg:w-[52px] group-hover:grayscale-0 grayscale transition-all", {
+                    "group-hover:opacity-100 opacity-30 md:!w-10": z.name === "Nervape",
+                  })}
+                />
+                <div className="text-base sm:text-2xl font-medium lg:text-4xl text-grey-200 group-hover:text-dark-100 transition-all">
+                  {z.name}
+                </div>
               </div>
-            </div>
-          ))}
+            );
+          })}
         </div>
       </div>
     </section>
